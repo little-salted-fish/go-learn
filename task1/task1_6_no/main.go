59494e947c794853e1459e87f22dda51ed68ab81
liujie <1910902213@qq.com>
2025-09-14 22:29:47 +0800

    添加题目

---
+++ task1/task1_6_no/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(removeDuplicates([]int{0, 1, 1, 3, 1, 32, 3}))
+
+}
+
+func removeDuplicates(nums []int) int {
+
+	return -1
+}
---
+++ task1/task1_7_no/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(merge([][]int{{1, 2}, {3, 4}}))
+
+}
+
+func merge(intervals [][]int) [][]int {
+
+	return [][]int{}
+}
