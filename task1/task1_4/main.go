7bc9fc4ffab464db472befe2143745e8c9f986df
liujie <1910902213@qq.com>
2025-09-15 21:56:09 +0800

    最长公共前缀官方写法

--- task1/task1_4/main.go
+++ task1/task1_4/main.go
@@ -13,11 +13,27 @@ func main() {
 
 }
 
-// 列循环对比
 func longestCommonPrefix(strs []string) string {
+	//官方写法，以下为自己写
+	if len(strs) == 0 {
+		return ""
+	}
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+	return strs[0]
+
+}
+
+// 列循环对比（优化可以去除一个计算最小长度的for循环）
+func longestCommonPrefix1(strArr []string) string {
 
 	var minLen = -1
-	for _, v := range strs {
+	for _, v := range strArr {
 		if minLen == -1 {
 			minLen = len(v)
 		} else if minLen > len(v) {
@@ -30,12 +46,13 @@ out:
 	for i := 0; i < minLen; i++ {
 
 		a := ""
-		for _, v := range strs {
+		for _, v := range strArr {
 			//fmt.Println(i, v[i:i+1])
 			if a == "" {
 				a = v[i : i+1]
 			} else if a == v[i:i+1] {
 				//prefix += a
+				//这步走没用可以跳过
 			} else {
 				break out
 			}
