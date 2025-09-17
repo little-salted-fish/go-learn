6f09b6fa891d87a461b61a24d5b1ca54ccc20999
liujie <1910902213@qq.com>
2025-09-18 00:20:54 +0800

    task2完成

---
+++ task2/task2_5/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+
+	lockAdd()
+	noLockAdd()
+
+}
+
+func lockAdd() {
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+
+	num := 0
+	mutex := sync.Mutex{}
+	for i := 0; i < 10; i++ {
+		go add1000(&num, &mutex, &wg)
+	}
+	wg.Wait()
+	fmt.Println("加锁结果", num)
+}
+func add1000(num *int, sync *sync.Mutex, wg *sync.WaitGroup) {
+	sync.Lock()
+	defer sync.Unlock()
+	defer wg.Done()
+
+	for i := 1; i <= 1000; i++ {
+		*num = *num + 1
+	}
+}
+
+func noLockAdd() {
+	wg1 := sync.WaitGroup{}
+	wg1.Add(10)
+
+	automic := atomic.Int64{}
+
+	for i := 0; i < 10; i++ {
+		go add1000NoLock(&automic, &wg1)
+	}
+	wg1.Wait()
+	fmt.Println("无锁结果", automic.Load())
+}
+
+func add1000NoLock(num *atomic.Int64, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	for i := 1; i <= 1000; i++ {
+		num.Add(1)
+	}
+}
